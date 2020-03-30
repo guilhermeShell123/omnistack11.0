9121b369828ba5cb8d148786f17a5b0e3a8fdc1a
guilhermeShell123 <guiga_regis@hotmail.com>
2020-03-30 14:41:37 -0300

    Validação dos requests

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,13 +1,14 @@
 const express = require('express');
 const cors = require('cors');
 const routes = require('./routes');
-
+const { errors } = require('celebrate');
 const app = express();
 
 
 app.use(cors());//origin: 'http://minhaplicação.com
 app.use(express.json());
 app.use(routes);
+app.use(errors());
 
 console.log('aplicação iniciada');
 app.listen(3333);
